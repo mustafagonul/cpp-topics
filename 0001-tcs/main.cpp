1b824b696eacc124e8a3b9f246defe87f7830e78
Mustafa Gönül <mustafagonul@gmail.com>
2018-07-24 20:34:44 +0300

    - 0001-tcs added

---
+++ 0001-tcs/main.cpp
@@ -0,0 +1,25 @@
+// This is not working
+/*
+template <typename Type>
+class foo {
+  static_assert(false, "Wrong Type");
+};
+*/
+
+template <typename Type, unsigned size = sizeof(Type)>
+class foo {
+  static_assert(sizeof(Type) == -1, "Wrong Type!");
+};
+
+template <>
+class foo<int> {
+};
+
+
+int main()
+{
+  foo<int> fi;    // should compile
+  foo<double> fd; // should not compile
+
+  return 0;
+}
