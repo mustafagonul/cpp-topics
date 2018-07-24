e3cd4f6064dd0841f6f0728a5d8a89180f637e9c
Mustafa Gönül <mustafagonul@gmail.com>
2018-07-24 21:04:43 +0300

    - 0001-tcs - example corrected, references are added

---
+++ 0001-tcs/README.md
@@ -0,0 +1,7 @@
+
+
+### References
+
+- https://stackoverflow.com/questions/30078818/static-assert-dependent-on-non-type-template-parameter-different-behavior-on-gc
+- https://stackoverflow.com/questions/17679268/enforce-template-type-through-static-assert
+- https://stackoverflow.com/questions/51504524/compile-error-for-class-template-except-its-specializations
--- 0001-tcs/main.cpp
+++ 0001-tcs/main.cpp
@@ -6,7 +6,7 @@ class foo {
 };
 */
 
-template <typename Type, unsigned size = sizeof(Type)>
+template <typename Type>
 class foo {
   static_assert(sizeof(Type) == -1, "Wrong Type!");
 };
@@ -19,7 +19,7 @@ class foo<int> {
 int main()
 {
   foo<int> fi;    // should compile
-  foo<double> fd; // should not compile
+  // foo<double> fd; // should not compile
 
   return 0;
 }
